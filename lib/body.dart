dcccb6f93b854be3f667ca53d33ec9f7097eedbe
Abdul Qadir Abdul Latif <92687658+qadirlatif@users.noreply.github.com>
2023-05-11 00:50:53 +0500

    code refactored of body.dart

--- lib/body.dart
+++ lib/body.dart
@@ -17,26 +17,32 @@ class Body extends StatelessWidget {
         children: [
           Row(
             children: [
-              widgetcaller.containerType1(
-                  "It's not faith in technology it's faith in ", "Technology"),
-              widgetcaller.containersType2(
+              widgetcaller.customContainerWidget(
+                  "It's not faith in technology it's faith in ",
+                  "Technology",
+                  0),
+              widgetcaller.customContainerWidget(
                   "Great Things in business are never down by one person",
-                  "Business"),
+                  "Business",
+                  25),
             ],
           ),
           Row(
             children: [
-              widgetcaller.containerType1("Design is not just what it looks like", "Design"),
-              widgetcaller.containersType2(
-                  "Everyone here has the sense that right", "Imagination")
+              widgetcaller.customContainerWidget(
+                  "Design is not just what it looks like", "Design", 0),
+              widgetcaller.customContainerWidget(
+                  "Everyone here has the sense that right", "Imagination", 25)
             ],
           ),
           Row(
             children: [
-              widgetcaller.containerType1(
-                  "Computers themselves, and software yet to be", "Computers"),
-              widgetcaller.containersType2(
-                  "Being the richest man is the cemenery doesn't", "Man")
+              widgetcaller.customContainerWidget(
+                  "Computers themselves, and software yet to be",
+                  "Computers",
+                  0),
+              widgetcaller.customContainerWidget(
+                  "Being the richest man is the cemenery doesn't", "Man", 25)
             ],
           )
         ],
@@ -46,7 +52,8 @@ class Body extends StatelessWidget {
 }
 
 class CustomWidget extends MaterialApp {
-  Widget containerType1(String qoute, String author) {
+  Widget customContainerWidget(
+      String qoute, String author, double marginfromtop) {
     return Container(
       decoration: BoxDecoration(
           border: Border.all(color: Colors.white),
@@ -61,51 +68,11 @@ class CustomWidget extends MaterialApp {
           borderRadius: BorderRadius.circular(10)),
       height: 150,
       width: 150,
-      margin: const EdgeInsets.fromLTRB(15, 0, 10, 10),
+      margin: EdgeInsets.fromLTRB(15, marginfromtop, 10, 10),
       child: Container(
         child: Column(
           children: [
-            Text(
-              qoute,
-              textAlign: TextAlign.left,
-              style: const TextStyle(
-                  fontSize: 18
-                  ),
-            ),
-            Text(
-              author,
-              textAlign: TextAlign.right,
-              style: const TextStyle(
-                fontSize: 17,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-              ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget containersType2(String qoute, String author) {
-    return Container(
-      decoration: BoxDecoration(
-          border: Border.all(color: Colors.white),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(0.0, 1.0), //(x,y)
-              blurRadius: 20.0,
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10)),
-      height: 150,
-      width: 150,
-      margin: const EdgeInsets.fromLTRB(15, 25, 10, 10),
-      child: Container(
-        child: Column(
-          children: [
+            const Text(""),
             Text(
               qoute,
               textAlign: TextAlign.left,
